50525fdad6a42b43907d1a18211553d96259e154
6im0n <simon.gl3135@orange.fr>
2024-02-13 18:37:10 +0100

    :sparkles: add elementary components

---
+++ include/Components/Elementary/Not.hpp
@@ -0,0 +1,18 @@
+/*
+** EPITECH PROJECT, 2024
+** MicroTekSpice
+** File description:
+** Not
+*/
+
+#pragma once
+#include "Acomponent.hpp"
+
+namespace nts {
+    class NotComponent : public Acomponent {
+    public:
+        NotComponent();
+        ~NotComponent();
+        nts::Tristate compute(std::size_t pin) override;
+    };
+}
