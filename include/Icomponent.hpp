271dec5c38cffa54da495ee9a9b6b779c3a4251b
6im0n <simon.gl3135@orange.fr>
2024-02-13 19:05:37 +0100

    :construction: update for better reading

--- include/Icomponent.hpp
+++ include/Icomponent.hpp
@@ -10,13 +10,9 @@
 #include <iostream>
 #include <map>
 #include <vector>
-namespace nts {
-    enum Tristate {
-        Undefined = (-true),
-        True = true,
-        False = false
-    };
+#include "Tristate.hpp"
 
+namespace nts {
     class IComponent {
     public:
         virtual ~IComponent() = default;
@@ -24,9 +20,6 @@ namespace nts {
         virtual nts::Tristate compute(std::size_t pin) = 0;
         virtual void setLink(std::size_t pin, nts::IComponent& other, std::size_t otherPin) = 0;
         virtual nts::Tristate getLink(std::size_t pin) = 0;
-
         virtual void setState(nts::Tristate state) = 0;
-    protected:
-    private:
     };
 }
