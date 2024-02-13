d0506767d5e58b1a3c279072d074cc5cd759c1da
6im0n <simon.gl3135@orange.fr>
2024-02-13 21:34:30 +0100

    :construction: refactor the xor and or components

--- src/Components/Elementary/Or.cpp
+++ src/Components/Elementary/Or.cpp
@@ -16,14 +16,10 @@ nts::Tristate nts::OrComponent::compute(std::size_t pin)
     if (pin == 3) {
         nts::Tristate state1 = getLink(1);
         nts::Tristate state2 = getLink(2);
-        if (state1 == nts::Tristate::True && state2 == nts::Tristate::True)
+        if (state1 == nts::Tristate::True || state2 == nts::Tristate::True)
             return nts::Tristate::True;
         else if (state1 == nts::Tristate::False && state2 == nts::Tristate::False)
             return nts::Tristate::False;
-        else if ((state1 == nts::Tristate::False && state2 == nts::Tristate::True) || (state1 == nts::Tristate::True && state2 == nts::Tristate::False))
-            return nts::Tristate::True;
-        else if ((state1 == nts::Tristate::Undefined && state2 == nts::Tristate::True) || (state1 == nts::Tristate::True && state2 == nts::Tristate::Undefined))
-            return nts::Tristate::True;
         else
             return nts::Tristate::Undefined;
     } else if (pin == 1 || pin == 2) {
--- src/Components/Elementary/Xor.cpp
+++ src/Components/Elementary/Xor.cpp
@@ -16,11 +16,11 @@ nts::Tristate nts::XorComponent::compute(std::size_t pin)
     if (pin == 3) {
         nts::Tristate state1 = getLink(1);
         nts::Tristate state2 = getLink(2);
-        if ((state1 == nts::Tristate::True && state2 == nts::Tristate::True) || (state1 == nts::Tristate::False && state2 == nts::Tristate::False))
+        if ( state1 == state2 && state1 != nts::Tristate::Undefined)
             return nts::Tristate::False;
-        else if ((state1 == nts::Tristate::False && state2 == nts::Tristate::True) || (state1 == nts::Tristate::True && state2 == nts::Tristate::False))
+        else if (state1 != state2 && state1 != nts::Tristate::Undefined && state2 != nts::Tristate::Undefined)
             return nts::Tristate::True;
-        else 
+        else
             return nts::Tristate::Undefined;
     } else if (pin == 1 || pin == 2) {
         return this->_links[pin]->compute(this->_pins[pin]);
