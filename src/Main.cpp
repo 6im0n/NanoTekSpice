693d198a763f25fb535cb9a3603feab174dc7c9c
Marius-P1 <marius.pain@epitech.eu>
2024-02-13 19:58:43 +0100

    ✨ Update Main.cpp and add NanoTekSpice.cpp

--- src/Main.cpp
+++ src/Main.cpp
@@ -5,9 +5,33 @@
 ** Main
 */
 
-#include "include.hpp"
+#include "Include.hpp"
+#include "Parsing/CircuitFile.hpp"
 
-int main(void)
+int display_help()
 {
+    std::cout << "\nUSAGE" << std::endl;
+    std::cout << "\t./nanotekspice [path_to_file]" << std::endl;
+    std::cout << "\nDESCRIPTION" << std::endl;
+    std::cout << "\tpath_to_file\tpath to the configuration file.\n" << std::endl;
+    return 0;
+}
+
+int main(int ac, char **av)
+{
+    if (ac == 2 && std::string(av[1]) == "-h") {
+        return display_help();
+    }
+    if (ac != 2) {
+        std::cerr << "Invalid number of arguments" << std::endl;
+        std::cerr << "Try './nanotekspice -h' for more information." << std::endl;
+        return 84;
+    }
+    try {
+        nts::CircuitFile circuit(av[1]);
+    } catch (nts::CircuitFile::FileError &e) {
+        std::cerr << e.what() << std::endl;
+        return 84;
+    }
     return 0;
 }
---
+++ src/NanoTekSpice.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2024
+** MicroTekSpice
+** File description:
+** NanoTekSpice
+*/
+
+#include "NanoTekSpice.hpp"
+
+NanoTekSpice::NanoTekSpice()
+{
+}
+
+NanoTekSpice::~NanoTekSpice()
+{
+}
