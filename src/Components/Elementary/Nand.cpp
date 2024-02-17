a8a3fb66493fc2eb767a45940abddc30453c1dae
6im0n <simon.gl3135@orange.fr>
2024-02-17 17:58:08 +0100

    🚧 add elemntary components

---
+++ src/Components/Elementary/Nand.cpp
@@ -0,0 +1,29 @@
+/*
+** EPITECH PROJECT, 2024
+** MicroTekSpice
+** File description:
+** Nand
+*/
+
+#include "Components/Elementary/Nand.hpp"
+
+nts::NandComponent::NandComponent() : Acomponent(3)
+{
+}
+
+nts::Tristate nts::NandComponent::compute(std::size_t pin)
+{
+    if (pin == 3) {
+        nts::Tristate state1 = getLink(1);
+        nts::Tristate state2 = getLink(2);
+        if ((state1 == nts::Tristate::False || state2 == nts::Tristate::False ) && state1 != nts::Tristate::True && state2 != nts::Tristate::True)
+            return nts::Tristate::True;
+        else if ((state1 == nts::Tristate::True || state2 == nts::Tristate::True) && state1 != nts::Tristate::Undefined && state2 != nts::Tristate::Undefined)
+            return nts::Tristate::False;
+        else
+            return nts::Tristate::Undefined;
+    } else if (pin == 1 || pin == 2) {
+        return this->_links[pin]->compute(this->_pins[pin]);
+    }
+    return nts::Tristate::Undefined;
+}
---
+++ src/Components/Elementary/Nor.cpp
@@ -0,0 +1,30 @@
+/*
+** EPITECH PROJECT, 2024
+** MicroTekSpice [SSH: 192.168.1.201]
+** File description:
+** Nor
+*/
+
+#include "Components/Elementary/Nor.hpp"
+
+nts::NorComponent::NorComponent() : Acomponent(3)
+{
+}
+
+nts::Tristate nts::NorComponent::compute(std::size_t pin)
+{
+    if (pin == 3) {
+        nts::Tristate state1 = getLink(1);
+        nts::Tristate state2 = getLink(2);
+        if (state1 == nts::Tristate::False && state2 == nts::Tristate::False)
+            return nts::Tristate::True;
+        else if (state1 == nts::Tristate::True || state2 == nts::Tristate::True)
+            return nts::Tristate::False;
+        else
+            return nts::Tristate::Undefined;
+
+    } else if (pin == 1 || pin == 2) {
+        return this->_links[pin]->compute(this->_pins[pin]);
+    }
+    return nts::Tristate::Undefined;
+}
