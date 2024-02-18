1fa1f2dc3208a4e874118acc7c2f583f9d1df4f4
Marius-P1 <marius.pain@epitech.eu>
2024-02-18 21:26:27 +0100

    ♻️ Refactor CircuitFile class to add getter methods for chipsets and links

--- src/Parsing/CircuitFile.cpp
+++ src/Parsing/CircuitFile.cpp
@@ -7,12 +7,22 @@
 
 #include "Parsing/CircuitFile.hpp"
 
+std::deque<std::pair<std::string, std::string>> nts::CircuitFile::getChipsets() const
+{
+    return this->_chipsets;
+}
+
+std::deque<std::pair<std::pair<std::string, int>, std::pair<std::string, int>>> nts::CircuitFile::getLinks() const
+{
+    return this->_links;
+}
+
 static void removeComment(std::deque<std::string> &fileContent)
 {
     for (size_t i = 0; i < fileContent.size(); i++) {
         if (fileContent[i].find("#") != std::string::npos)
             fileContent[i] = fileContent[i].substr(0, fileContent[i].find("#"));
-        Utils::trimEnd(fileContent[i]);
+        Utils::trim(fileContent[i]);
         if (fileContent[i].empty()) {
             fileContent.erase(fileContent.begin() + i);
             i--;
@@ -27,7 +37,7 @@ static std::pair<std::string, std::string> getChipset(std::string line)
     std::smatch match;
 
     if (!std::regex_match(line, match, reg))
-        throw nts::CircuitFile::FileError("Error: invalid chipset line");
+        throw nts::Error("Error: invalid chipset line");
     chipset.first = match[1];
     chipset.second = match[2];
     return chipset;
@@ -36,7 +46,7 @@ static std::pair<std::string, std::string> getChipset(std::string line)
 void nts::CircuitFile::readChipsets(std::deque<std::string> &fileContent, size_t &pos)
 {
     if (fileContent[pos] != ".chipsets:")
-        throw nts::CircuitFile::FileError("Error: .chipsets: not found");
+        throw nts::Error("Error: .chipsets: not found");
     for (pos = 1; pos < fileContent.size() && fileContent[pos] != ".links:"; pos++) {
         this->_chipsets.push_back(getChipset(fileContent[pos]));
     }
@@ -49,14 +59,14 @@ static std::pair<std::pair<std::string, int>, std::pair<std::string, int>> getLi
     std::smatch match;
 
     if (!std::regex_match(line, match, reg))
-        throw nts::CircuitFile::FileError("Error: invalid link line");
+        throw nts::Error("Error: invalid link line");
     try {
         link.first.first = match[1];
         link.first.second = std::stoi(match[2]);
         link.second.first = match[3];
         link.second.second = std::stoi(match[4]);
     } catch (std::exception &e) {
-        throw nts::CircuitFile::FileError("Error: invalid link line");
+        throw nts::Error("Error: invalid link line");
     }
     return link;
 }
@@ -64,7 +74,7 @@ static std::pair<std::pair<std::string, int>, std::pair<std::string, int>> getLi
 void nts::CircuitFile::readLinks(std::deque<std::string> &fileContent, size_t &pos)
 {
     if (pos >= fileContent.size() || fileContent[pos] != ".links:")
-        throw nts::CircuitFile::FileError("Error: .links: not found");
+        throw nts::Error("Error: .links: not found");
     for (pos = pos + 1; pos < fileContent.size(); pos++) {
         this->_links.push_back(getLink(fileContent[pos]));
     }
@@ -76,7 +86,7 @@ static void readLinesFromFile(std::string file, std::deque<std::string> &fileCon
     std::string line;
 
     if (!ifs.is_open())
-        throw nts::CircuitFile::FileError("Error: " + file + " file not found");
+        throw nts::Error("Error: " + file + " file not found");
     while (std::getline(ifs, line)) {
         fileContent.push_back(line);
     }
@@ -89,19 +99,9 @@ nts::CircuitFile::CircuitFile(std::string file)
     size_t pos = 0;
 
     if (file.substr(file.find_last_of(".") + 1) != "nts")
-        throw nts::CircuitFile::FileError("Error: invalid file extension (should be .nts)");
+        throw nts::Error("Error: invalid file extension (should be .nts)");
     readLinesFromFile(file, fileContent);
     removeComment(fileContent);
     readChipsets(fileContent, pos);
     readLinks(fileContent, pos);
-}
-
-nts::CircuitFile::FileError::FileError(std::string const &message)
-{
-    this->_message = message;
-}
-
-const char *nts::CircuitFile::FileError::what() const noexcept
-{
-    return this->_message.c_str();
 }
