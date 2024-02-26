26502df7950c23f9f308a4c00855a51c1c766e0c
6im0n <simon.gl3135@orange.fr>
2024-02-26 15:39:10 +0100

    🚧 Add C4514 class and header for the 4514

--- include/Components/Advanced/4514.hpp
+++ include/Components/Advanced/4514.hpp
@@ -7,3 +7,14 @@
 
 #pragma once
 
+#include <array>
+#include "Components/AChipset.hpp"
+
+namespace nts {
+    class C4514 : public AChipset {
+    public:
+        C4514(std::string name = "");
+        ~C4514() = default;
+        nts::Tristate compute(std::size_t pin) override;
+    };
+}
