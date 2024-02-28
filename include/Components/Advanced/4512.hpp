56e37d98fc69101e16cca74ea9552089ee319f40
Marius-P1 <marius.pain@epitech.eu>
2024-02-28 12:57:37 +0100

    ✨ Add 4512 component implementation

--- include/Components/Advanced/4512.hpp
+++ include/Components/Advanced/4512.hpp
@@ -7,3 +7,15 @@
 
 #pragma once
 
+#include "Components/AComponent.hpp"
+
+namespace nts {
+    class C4512 : public AComponent {
+      public:
+        C4512(std::string name = "");
+        ~C4512() = default;
+        nts::Tristate compute(size_t pin) override;
+        nts::Tristate getOutput(void);
+    };
+
+}
