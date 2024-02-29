1de6549148ff9d162c7f0d51ecd9f2ecda952a93
6im0n <simon.gl3135@orange.fr>
2024-02-29 16:07:54 +0100

    ✨ Add C4017 component implementation

--- include/Components/Advanced/4017.hpp
+++ include/Components/Advanced/4017.hpp
@@ -7,3 +7,21 @@
 
 #pragma once
 
+#include <array>
+#include "Components/AComponent.hpp"
+
+namespace nts {
+    class C4017 : public AComponent {
+    public:
+        C4017(std::string name = "");
+        ~C4017() = default;
+        nts::Tristate compute(std::size_t pin) override;
+        void updateState(void);
+        void resetState(void);
+    private:
+        std::array<nts::Tristate, 11> _out;
+        std::map<int, int> _pinMap;
+        nts::Tristate _prevClock;
+        int _prevValue;
+    };
+}
