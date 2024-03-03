5edf1222bfea2d99eb9af001b97f24f7243adea9
6im0n <simon.gl3135@orange.fr>
2024-03-03 22:55:56 +0100

    🔥 Remove test_TestGatesComponents.cpp

--- tests/Components/test_TestGatesComponents.cpp
+++
@@ -1,8 +0,0 @@
-Test(C4011, testWrongPin2)
-{
-    nts::C4011 c4011comp("test");
-
-    cr_assert_eq(c4011comp.compute(7), nts::Tristate::Undefined);
-    cr_assert_eq(c4011comp.compute(14), nts::Tristate::Undefined);
-    cr_assert_eq(c4011comp.compute(99), nts::Tristate::Undefined);
-}
