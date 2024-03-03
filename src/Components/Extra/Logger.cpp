7aea96cae3aa85b8ed1e4f9c3f79ac825ee41e64
6im0n <simon.gl3135@orange.fr>
2024-03-03 19:09:39 +0100

    ✨ Add byte logging functionality to Logger component

--- src/Components/Extra/Logger.cpp
+++ src/Components/Extra/Logger.cpp
@@ -5,8 +5,34 @@
 ** Logger
 */
 
+/*
+ Byte Logger Component
+
+           +-Logger-+
+           |        |
+ in_001 -1->        |
+           |        |
+ in_002 -2->        |
+           |        |
+ in_004 -3->        |
+           |        |
+ in_008 -4->        |
+           |        |
+ in_016 -5->        |
+           |        |
+ in_032 -6->        |
+           |        |
+ in_064 -7->        |
+           |        |
+ in_128 -8->        |
+           |        |
+           +-^----^-+
+             |    |
+    clock -9-+    +-10- inhibit
+*/
 
 #include "Components/Extra/Logger.hpp"
+#include <fstream>
 
 nts::Logger::Logger(std::string name) : AComponent(10, name)
 {
@@ -15,24 +41,36 @@ nts::Logger::Logger(std::string name) : AComponent(10, name)
 
 nts::Tristate nts::Logger::compute(std::size_t pin)
 {
-    std::vector<nts::Tristate> inputs = {getLink(1), getLink(2), getLink(3), getLink(4),
+    std::vector<nts::Tristate> inputs =
+        {getLink(1), getLink(2), getLink(3), getLink(4),
         getLink(5), getLink(6), getLink(7), getLink(8)};
     nts::Tristate clock = getLink(9);
     nts::Tristate inhibit = getLink(10);
-    size_t value = 0;
+    std::ofstream file;
+    char value = 0;
 
-    if (pin == 11)
-        std::cout << "Logger: " << this->getName() << std::endl; // TODO Add logics
-    if (pin == 0 || pin == 10 || pin > this->_pins.size())
+    if (pin == 0 || pin > this->_pins.size())
         return nts::Tristate::Undefined;
-    if (inhibit == nts::Tristate::True || inhibit == nts::Tristate::Undefined)
+    if (pin == 10 || pin == 9)
+        return this->_links[pin]->compute(this->_pins[pin]);
+    if (inhibit == nts::Tristate::True || inhibit == nts::Tristate::Undefined) {
+        this->_prevClock = clock;
         return nts::Tristate::False;
-    if (clock == nts::Tristate::True && this->_prevClock == nts::Tristate::False) {
+    }
+    for (size_t i = 0; i < inputs.size(); i++) {
+        if (inputs[i] == nts::Tristate::Undefined) {
+            this->_prevClock = clock;
+            return nts::Tristate::Undefined;
+        }
+    }
+    if (clock == nts::Tristate::True && this->_prevClock == nts::Tristate::False && pin == 11) {
         for (size_t i = 0; i < inputs.size(); i++) {
             value += (inputs[i] == nts::Tristate::True) ? 1 << i : 0;
         }
-        std::cout << value << std::endl;
+        file.open("./log.bin", std::ios::app);
+        file << value;
+        file.close();
     }
     this->_prevClock = clock;
-    return nts::Tristate::False;
+    return nts::Tristate::True;
 }
